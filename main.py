f5c972960770303abadffafa44c1b3a0879e6264
hzunair <hzunair@think-bricks.com>
2019-04-28 12:57:14 +0600

    Upload files

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, flash, redirect, render_template, request, session, abort
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello world! I am Hasib."
+
+if __name__ == "__main__":
+    app.run()
+    #app.run(host='0.0.0.0', port=80)
